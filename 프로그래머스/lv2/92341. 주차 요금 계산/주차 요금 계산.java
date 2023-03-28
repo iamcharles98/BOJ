7552e650113edffcd1e6357fe776ca9c2828ce06
ChansooJeong <77047099+iamcharles98@users.noreply.github.com>
2023-03-28 12:47:42 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.53 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,64 +2,70 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-      int[] answer;
-        HashMap<String, Integer> parking = new HashMap<>();
-        HashMap<String, Integer> fee = new HashMap<>();
-
-        for (String r : records)
+        HashMap<Integer,String> In_time = new HashMap<>();
+        HashMap<Integer,Integer> Price = new HashMap<>();
+        for(String record : records)
         {
-            String[] record = r.split(" ");
-            int t = getMin(record[0]);
-            String n = record[1];
-            String io = record[2];
-
-            if(io.equals("IN"))
+            String[] temp = record.split(" ");
+            int car_num = Integer.parseInt(temp[1]);
+            String flag = temp[2];
+            if(flag.equals("IN"))
             {
-                parking.put(n, t);
+                String in_time = temp[0];
+                In_time.put(car_num,in_time);
+                if(!Price.containsKey(car_num))Price.put(car_num,0);
             }
             else
             {
-                if(!fee.containsKey(n))
-                {
-                    fee.put(n, t - parking.get(n));
-                }
-                else
-                {
-                    fee.put(n, fee.get(n) + t - parking.get(n));
-                }
-                parking.remove(n);
+                String out_time = temp[0];
+                int time = calculator2(In_time.get(car_num),out_time);
+                Price.put(car_num,Price.get(car_num)+time);
+                In_time.remove(car_num);
+
             }
         }
+        if(!In_time.isEmpty())
+        {
+            for (Integer integer : In_time.keySet()) {
+                int time = calculator2(In_time.get(integer),"23:59");
+                Price.put(integer,Price.get(integer)+time);
+                In_time.remove(integer);
 
-
-        for (String key : parking.keySet()) {
-            if(!fee.containsKey(key)) {
-                fee.put(key, (23*60+59)-parking.get(key));
-            }else {
-                fee.put(key, (23*60+59)-parking.get(key)+fee.get(key));
             }
         }
-
-
-        Map<String, Integer> sortedFee = new TreeMap<>(fee);
-
-
-        answer = new int[fee.size()];
-        int i = 0;
-        for (String car : sortedFee.keySet()) {
-            int min = sortedFee.get(car);
-
-            if(min<=fees[0]) {
-                answer[i++] = fees[1];
-            }else {
-                answer[i++] = (int) (fees[1] + Math.ceil((double)(min-fees[0])/fees[2])*fees[3]) ;
-            }
+        for(int i : Price.keySet())
+        {
+            Price.put(i,calculator(Price.get(i),fees));
+        }
+        int [] answer;
+        List<Integer> keyset = new ArrayList<>(Price.keySet());
+        answer = new int[keyset.size()];
+        int cnt = 0;
+        Collections.sort(keyset);
+        for(int it : keyset)
+        {
+            answer[cnt++]=Price.get(it);
         }
         return answer;
     }
+     
+    private static int calculator2(String inTime, String outTime) {
+        int IntimeTomin = Integer.parseInt(inTime.split(":")[0])*60 + Integer.parseInt(inTime.split(":")[1]);
+        int OuttimeTomin = Integer.parseInt(outTime.split(":")[0])*60 + Integer.parseInt(outTime.split(":")[1]);
+        int result = OuttimeTomin-IntimeTomin;
+        return result;
+    }
+    
+    private static int calculator(int totalTime, int[] fees) {
+        int std_time = fees[0];
+        int std_fee = fees[1];
+        int plus_time = fees[2];
+        int plus_fee = fees[3];
 
-    public static int getMin(String time) {
-        String[] t = time.split(":");
-        return Integer.parseInt(t[0])*60 + Integer.parseInt(t[1]);
+        int fee = std_fee;
+        double value = (double) (totalTime-std_time)/(double) plus_time;
+        value = Math.ceil(value);
+        if(totalTime>std_time) fee =  std_fee + (int)value*plus_fee;
+        return fee;
     }
 }
