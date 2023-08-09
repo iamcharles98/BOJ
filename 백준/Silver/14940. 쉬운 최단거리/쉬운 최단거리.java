6760f4f7bb91c3c80f918a72c11630dc532e8a94
ChansooJeong <77047099+iamcharles98@users.noreply.github.com>
2023-08-09 17:54:40 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 2872 ms, Memory: 91152 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,91 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static Integer N;
+    static Integer M;
+
+    static Integer[][] MAP;
+
+    static Integer[][] DISTANCE;
+    static node TARGET_PLACE;
+
+    static Integer[] dx = {0, 0, -1, 1};
+    static Integer[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        M = Integer.parseInt(stringTokenizer.nextToken());
+        MAP = new Integer[N][M];
+        DISTANCE = new Integer[N][M];
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < M; j++) {
+                MAP[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                DISTANCE[i][j] = Integer.MAX_VALUE;
+                // 2 는 목표지점
+                if (MAP[i][j] == 2) {
+                    TARGET_PLACE = new node(i, j);
+                    DISTANCE[i][j] = 0;
+                }
+                // 0 은 갈 수 없는 땅
+                if (MAP[i][j] == 0) {
+                    DISTANCE[i][j] = 0;
+                }
+            }
+        }
+
+        findDistance();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (DISTANCE[i][j] == Integer.MAX_VALUE) {
+                    System.out.print(-1 + " ");
+                }
+                else {
+                    System.out.print(DISTANCE[i][j] + " ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void findDistance() {
+        Queue<node> queue = new LinkedList<>();
+        queue.add(TARGET_PLACE);
+        while (!queue.isEmpty()) {
+            node cur_node = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nRow = cur_node.row + dy[i];
+                int nCol = cur_node.col + dx[i];
+                if (nRow < 0 || nRow >= N || nCol < 0 || nCol >= M || MAP[nRow][nCol] == 0) {
+                    continue;
+                }
+                int dist = DISTANCE[cur_node.row][cur_node.col] + 1;
+                if (DISTANCE[nRow][nCol] > dist) {
+                    DISTANCE[nRow][nCol] = dist;
+                    queue.offer(new node(nRow, nCol));
+                }
+            }
+        }
+    }
+
+    private static class node {
+        int row;
+        int col;
+
+        public node(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+}
