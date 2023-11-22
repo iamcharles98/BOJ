8e7f0657a213258eff05008024a13606b6916788
ChansooJeong <77047099+iamcharles98@users.noreply.github.com>
2023-11-22 17:17:04 +0900

    [level 2] Title: [3차] n진수 게임, Time: 19.13 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,31 +1,17 @@
 import java.util.*;
 class Solution {
-    /*
-    진법 n
-    튜브가 말해야할 숫자의 갯수 t
-    게임 참가 인원 m
-    튜브의 순서 p
-    
-    String 배열 m * t 개 만들어서 0 ~ 배열 끝까지 진법에 따른 숫자 한개씩 저장
-    
-    */
-    Map<Integer, String> nStringMap = new HashMap<>();
     int N;
+    
     public String solution(int n, int t, int m, int p) {
         N = n;
-        initMap();
         String allNum = makeGameNumber(m*t);
         String result = "";
-        for(int turn=p-1; turn < allNum.length() && result.length() < t; turn+=m) {
+        for(int turn=p-1; result.length() < t; turn+=m) {
             result += Character.toString(allNum.charAt(turn));
         }
         return result;
     }
-    private void initMap() {
-        for(int i=0; i<16;i++) {
-            nStringMap.put(i,Integer.toString(i,16).toUpperCase());
-        }
-    }
+    
     private String makeGameNumber(int size) {
         String result = "";
         int num = 0;
@@ -33,18 +19,9 @@ class Solution {
             result += numToStringOfN(num);
             num++;
         }
-        return result;
+        return result.toUpperCase();
     }
     private String numToStringOfN(int number) {
-        String result = "";
-        if(number == 0) {
-            return nStringMap.get(number);
-        }
-        while(number > 0) {
-            result =  nStringMap.get(number % N)+ result;
-            number /= N;
-        }
-        
-        return result;
+        return Integer.toString(number,N);
     }
 }
