b104bcd8968e4048c34aecda88f434995eea953b
ChansooJung <77047099+iamcharles98@users.noreply.github.com>
2024-09-25 23:14:45 +0900

    [Gold V] Title: 리모컨, Time: 576 ms, Memory: 299316 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,90 +1,56 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    static Integer minMove = Integer.MAX_VALUE;
-    static Integer curChannel = 100;
-    static Integer targetChannel;
-
-    static Integer forbiddenNum;
-    static List<String> availNumList = new ArrayList<>();
-
-    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int goToChannel;
+    static int N;
+    static int CURRENT = 100;
+    static int minClickCnt = Integer.MAX_VALUE;
+    static Set<Integer> banned = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
+
+        goToChannel = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        if (N != 0) {
+            Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).forEach(i -> banned.add(i));
+        }
+        if (goToChannel == CURRENT) {
+            System.out.println(0);
+            return;
+        }
+        minClickCnt = Math.abs(goToChannel - CURRENT);
+
+        recur("");
+
+        System.out.println(minClickCnt);
+    }
+
+    private static void recur(String channel) {
+        if (channel.length() > 6) {
+            return;
+        }
+        if (!channel.isEmpty()) {
+            int dist = calcDist(channel) + channel.length();
+            if (dist < minClickCnt) {
+                minClickCnt = dist;
+            }
+        }
         for (int i = 0; i < 10; i++) {
-            availNumList.add(Integer.toString(i));
-        }
-        targetChannel = Integer.parseInt(bufferedReader.readLine());
-        forbiddenNum = Integer.parseInt(bufferedReader.readLine());
-        if (forbiddenNum != 0) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            for (int i = 0; i < forbiddenNum; i++) {
-                availNumList.remove(stringTokenizer.nextToken());
+            if (banned.contains(i)) {
+                continue;
             }
+            String nChannel = channel + i;
+            recur(nChannel);
         }
-
-        System.out.println(solution());
-
     }
 
-    private static Integer solution() {
-        int decreaseNum;
-        int increaseNum;
-        minMove = getMove(curChannel);
-        if (availNumList.isEmpty() || minMove == 0) {
-            return minMove;
-        }
-
-        int temp = targetChannel;
-        while (getMove(temp) <= minMove) {
-            if (Find(temp)) {
-                decreaseNum = getCount(temp) + getMove(temp);
-                if (minMove >= decreaseNum) {
-                    minMove = decreaseNum;
-                }
-                break;
-            }
-            temp -= 1;
-        }
-
-        temp = targetChannel;
-
-        while (getMove(temp) <= minMove) {
-            if (Find(temp)) {
-                increaseNum = getCount(temp) + getMove(temp);
-                if (minMove >= increaseNum) {
-                    minMove = increaseNum;
-                }
-                break;
-            }
-            temp += 1;
-        }
-
-        return minMove;
-
-    }
-
-    private static int getCount(int temp) {
-        return Integer.toString(temp).length();
-    }
-
-    private static boolean Find(int channel) {
-        String stringChannel = Integer.toString(channel);
-        String[] array = stringChannel.split("");
-        for (String string : array) {
-            if (!availNumList.contains(string)) return false;
-        }
-        return true;
-    }
-
-    private static Integer getMove(int cur) {
-        return Math.abs(targetChannel - cur);
+    private static Integer calcDist(String channel) {
+        return Math.abs(Integer.parseInt(channel) - goToChannel);
     }
 }
