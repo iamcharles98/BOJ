1727a1debf4c3169de9478b75393639ad8120d69
ChansooJung <77047099+iamcharles98@users.noreply.github.com>
2024-10-02 20:36:48 +0900

    [level 2] Title: H-Index, Time: 0.92 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,54 +1,19 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int n = citations.length;
-        
-        Arrays.sort(citations);
-        
-        for(int i=1; i<=n; i++){
-            int less = findLessThanH(citations, i);
-            int more = n - (findMoreThanH(citations, i) + 1);
-            
-            if(less <= i && more>=i) {
-                answer = i;
-            }
-        }
-            
-        
-        return answer;
-    }
-    private int findMoreThanH(int [] arr, int h) {
-        int left = 0;
-        int right = arr.length-1;
-        
-        while(left <= right) {
-            int mid = (left+right) / 2;
-            
-            
-            if(arr[mid] >= h) {
-                right = mid-1;
-            } else {
-                left = mid +1;
-            }
-        }
-        return right;
-    }
-    
-    private int findLessThanH(int [] arr, int h) {
-        int left = 0;
-        int right = arr.length-1;
-        
-        while(left <= right) {
-            int mid = (left+right) / 2;
+        int length = citations.length;
 
-            if(arr[mid] <= h) {
-                left = mid+1;
-            } else {
-                right = mid-1;
+        Arrays.sort(citations);
+
+        for (int i = length - 1; 0 <= i; i--){
+            if (length - i <= citations[i]) {
+                answer = length - i;
             }
-        }
-        return left;
+        };
+
+
+        return answer;
     }
 }
