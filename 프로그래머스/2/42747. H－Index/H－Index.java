b3075ee44b3054bf3b12b6eba37dc127f7855d79
ChansooJung <77047099+iamcharles98@users.noreply.github.com>
2024-10-02 20:23:15 +0900

    [level 2] Title: H-Index, Time: 1.93 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,26 +3,52 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        int n = citations.length;
         
-        int left = 0;
-        int right = 10000;
-        while(left < right) {
+        Arrays.sort(citations);
+        
+        for(int i=1; i<=n; i++){
+            int less = findLessThanH(citations, i);
+            int more = n - (findMoreThanH(citations, i) + 1);
             
-            int h = (left+right)/2;
-            int cnt = 0;
-            for(int cite : citations) {
-                if(cite >= h) {
-                    cnt++;
-                }
+            if(less <= i && more>=i) {
+                answer = i;
             }
-            if(cnt >= h) {
-                answer = h;
-                left = h+1;
-            } else {
-                 right = h;
-             }
-
         }
+            
+        
         return answer;
+    }
+    private int findMoreThanH(int [] arr, int h) {
+        int left = 0;
+        int right = arr.length-1;
+        
+        while(left <= right) {
+            int mid = (left+right) / 2;
+            
+            
+            if(arr[mid] >= h) {
+                right = mid-1;
+            } else {
+                left = mid +1;
+            }
+        }
+        return right;
+    }
+    
+    private int findLessThanH(int [] arr, int h) {
+        int left = 0;
+        int right = arr.length-1;
+        
+        while(left <= right) {
+            int mid = (left+right) / 2;
+
+            if(arr[mid] <= h) {
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+        return left;
     }
 }
