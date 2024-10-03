6a22d1335177feac7dc860009dc1952a6c4dd6bb
ChansooJung <77047099+iamcharles98@users.noreply.github.com>
2024-10-03 15:41:01 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 42.84 ms, Memory: 215 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,62 +1,64 @@
 import java.util.*;
+/*
+N*M => 최대 1,000,000
+공격 갯수 => 250,000
+행렬을 직접적으로 업데이트 하면 안됨
+skill을 모두 반영한 행렬을 하나 만들고 그것을 딱 반영했을 때, 몇개 살아남았는가 를 판별
+스킬을 반영할때도 O(N^2)가 걸리면 안됨
+
+1, 1 => 3,3 까지 2를 회복
+
+2 2 2    (r1,r2)2 0 0 -2 (r1,c2+1)
+2 2 2    0 0 0 0
+2 2 2    0 0 0 0
+         (c1+1, r2)-2     2 (c1+1,c2+1)
+         
+         
+         2차원에서도 =>(a,b) => (c,d) 까지 공격한다 가정하면 양 끝점만 변경해주면 됨
+         
+         마지막에 1,1 부터 n,n 까지 누적합을 구해주면 총 공격 및 회복 결과가 나옴
+         
+*/
 class Solution {
-    /*
-    각 칸마다 내구도
-    공격 -> 내구도가 0이하가 되면 파괴
-    회복 스킬 -> 내구도 상승
-    공격 및 회복 -> 직사각형 모양
-    공격과 회복 반복
-    파괴되지 않은 건물의 수를 반환
-    맵의 크기 1000 * 1000 -> 1,000,000
-    내구도 최대 1,000
-    skill -> 최대 250,000개
-    
-    공격 및 회복을 실제로 업데이트하면 시간초과
-    스킬에 대해서만 처리해주어야 할듯
-    [0,0,0,0] 1 ~ 2 -1 
-    */
-    int [][] attackAndRecovery;
-    public int solution(int[][] board, int[][] skill) {
+    public int solution(int[][] board, int[][] skills) {
         int answer = 0;
-        attackAndRecovery = new int[board.length+1][board[0].length+1];
+        int[][] score = new int[board.length+1][board[0].length+1];
         
-        for(int [] action : skill) {
-            process(action);
+        for(int [] skill : skills) {
+            int type = skill[0];
+            int r1 = skill[1];
+            int r2 = skill[2];
+            int c1 = skill[3];
+            int c2 = skill[4];
+            int degree = skill[5];
+            
+            degree = (type == 1) ? degree*(-1) : degree;
+            
+            score[r1][r2] += degree;
+            score[r1][c2+1] += degree*(-1);
+            score[c1+1][r2] += degree*(-1);
+            score[c1+1][c2+1] += degree;
         }
-        for(int row = 0; row<attackAndRecovery.length; row++) {
-            int sum = 0;
-            for(int col=0; col<attackAndRecovery[row].length; col++) {
-                sum += attackAndRecovery[row][col];
-                attackAndRecovery[row][col] = sum;
+        
+        for(int i=1; i< score.length; i++) {
+            score[0][i] += score[0][i-1];
+            score[i][0] += score[i-1][0];
+        }
+        
+        for(int i=1; i< score.length-1; i++) {
+            for(int j=1; j<score[i].length-1; j++) {
+                score[i][j] += score[i-1][j] + score[i][j-1] - score[i-1][j-1];
             }
         }
-        for(int col = 0; col<attackAndRecovery[0].length; col++) {
-            int sum = 0;
-            for(int row=0; row<attackAndRecovery.length; row++) {
-                sum += attackAndRecovery[row][col];
-                attackAndRecovery[row][col] = sum;
-            }
-        }
-        for(int row = 0; row<board.length; row++) {
-            for(int col=0; col<board[row].length; col++) {
-                if(board[row][col] +attackAndRecovery[row][col] > 0) {
-                    answer ++;
+        
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[i].length; j++) {
+                if(board[i][j] + score[i][j] > 0) {
+                    answer++;
                 }
             }
         }
-       
+        
         return answer;
-    }
-    private void process(int[] action) {
-        int type = action[0];
-        int startRow = action[1];
-        int startCol = action[2];
-        int endRow = action[3];
-        int endCol = action[4];
-        int degree = type == 1 ? action[5]*-1 : action[5];
-        attackAndRecovery[startRow][startCol] += degree;
-        attackAndRecovery[startRow][endCol+1] += degree*(-1);
-        attackAndRecovery[endRow+1][startCol] += degree*(-1);
-        attackAndRecovery[endRow+1][endCol+1] += degree;
     }
 }
