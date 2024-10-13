99ae2d179669c7cfa176824d207765cbbb8f8e06
ChansooJung <77047099+iamcharles98@users.noreply.github.com>
2024-10-13 19:49:55 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 292 ms, Memory: 49452 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Queue;
+
+public class Main {
+
+    static int N, M;
+    static int[] pos = new int[100001];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static class Move implements Comparable<Move> {
+        int pos;
+        int count;
+
+        public Move(int pos, int count) {
+            this.pos = pos;
+            this.count = count;
+        }
+
+        @Override
+        public int compareTo(Move o) {
+            return count - o.count;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] strings = br.readLine().split(" ");
+        N = Integer.parseInt(strings[0]);
+        M = Integer.parseInt(strings[1]);
+
+        if (M <= N) {
+            System.out.println(N - M);
+            return;
+        }
+
+        Queue<Move> queue = new PriorityQueue<>();
+        queue.add(new Move(N, 0));
+        Arrays.fill(pos, Integer.MAX_VALUE);
+
+        int minTime = M - N;
+        while (!queue.isEmpty()) {
+            Move cur = queue.poll();
+
+            if (cur.pos >= M) {
+                minTime = Math.min(minTime, cur.count + cur.pos - M);
+                continue;
+            }
+
+            if (cur.count > pos[cur.pos]) {
+                continue;
+            }
+
+            pos[cur.pos] = cur.count;
+
+            queue.add(new Move(cur.pos + 1, cur.count + 1));
+            if (cur.pos != 0) {
+                queue.add(new Move(cur.pos - 1, cur.count + 1));
+                queue.add(new Move(cur.pos * 2, cur.count));
+            }
+        }
+
+        System.out.println(minTime);
+
+    }
+
+
+}
